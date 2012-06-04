e01e3e7b2aa15184cce3c60fdefebb566a7e5ce4
Attila Domokos <adomokos@gmail.com>
2012-06-04 09:05:48 -0400

    Using the correct configuration key

--- start.js
+++ start.js
@@ -4,8 +4,8 @@ var Twitter = require('ntwitter')
 var twit = new Twitter({
   consumer_key: config.twitter.consumer_key,
   consumer_secret: config.twitter.consumer_secret,
-  access_token_key: access_token_key,
-  access_token_secret: access_token_secret
+  access_token_key: config.twitter.access_token_key,
+  access_token_secret: config.twitter.access_token_secret
 });
 
 twit.verifyCredentials(function (err, data) {
