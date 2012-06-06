31adfd12ca830304c43e8f79d8e7441e66fff607
Attila Domokos <adomokos@gmail.com>
2012-06-05 22:55:06 -0400

    Adding a few models and its builders. Testing is near.

--- terminal-twitter.js
+++ terminal-twitter.js
@@ -21,19 +21,21 @@ twitter.verifyCredentials(function (err, data) {
 Program
   .version('0.0.1')
   .option('-m, --mentions', 'Pulls the latest 5 mentions')
-  .option('-t, --timeline', 'The latest home timeline')
-  .option('-u, --update_status [text]', 'Pushes a status update to Twitter')
   .option('-r, --rate_limit', 'Twitter\'s rate limit status')
+  .option('-t, --timeline', 'The latest home timeline, filtered')
+  .option('-x, --timeline_unfiltered', 'The latest home timeline, unfiltered')
+  .option('-u, --update_status [text]', 'Pushes a status update to Twitter')
   .parse(process.argv);
 
 if (process.argv.length === 2) {
-  GetsHomeTimeline.forUser(twitter);
+  GetsHomeTimeline.withFilter(twitter);
 }
 else {
   if (Program.mentions) GetsMentions.forUser(twitter);
-  if (Program.timeline) GetsHomeTimeline.forUser(twitter);
-  if (Program.update_status) UpdatesStatus.withText(Program.update_status, twitter);
   if (Program.rate_limit) GetsRateLimit.forUser(twitter);
+  if (Program.timeline) GetsHomeTimeline.withFilter(twitter);
+  if (Program.timeline_unfiltered) GetsHomeTimeline.withoutFilter(twitter);
+  if (Program.update_status) UpdatesStatus.withText(Program.update_status, twitter);
 }
 
 //twitter.stream('user', '', function(stream) {
